9d70bd5d6356ce4f6b40b87f819cc57e094e46e6
robertderidder <c.d.ridder@student.tue.nl>
2026-01-22 12:11:26 +0000

    add phantom

--- zea/beamform/phantoms.py
+++ zea/beamform/phantoms.py
@@ -140,3 +140,31 @@ def golden_ratio(num_scatterers=100):
     scat_y = np.zeros_like(scat_x)
 
     return np.stack([scat_x, scat_y, scat_z], axis=1)
+
+def circle(n_points=50, radius=0.01, z_pos=22e-3):
+    """Generates a circular phantom of scatterers in the x-y plane.
+
+    Args:
+        radius (float): The radius of the circle in meters.
+        n_points (int): The number of scatterer points to generate.
+        z_pos (float): The z-coordinate for all scatterers.
+
+    Returns:
+        ndarray: The scatterer positions of shape (n_points, 3).
+    """
+    angles = np.linspace(0, 2 * np.pi, n_points, endpoint=False)
+    x = radius * np.cos(angles)
+    z = radius * np.sin(angles)
+    y = np.zeros_like(x)
+    z = z+z_pos
+    return np.stack([x, y, z], axis=1)
+
+def grid(n_x=5, n_z=5, x_range=(-10e-3, 10e-3), z_range=(15e-3, 30e-3)):
+    """Returns a grid of point scatterers."""
+    x = np.linspace(x_range[0], x_range[1], n_x)
+    z = np.linspace(z_range[0], z_range[1], n_z)
+    X, Z = np.meshgrid(x, z)
+    scat_x = X.flatten()
+    scat_y = np.zeros_like(scat_x)
+    scat_z = Z.flatten()
+    return np.stack([scat_x, scat_y, scat_z], axis=1)
